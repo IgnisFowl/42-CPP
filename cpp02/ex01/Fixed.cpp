d784f6fe3e5a58bad9ad38e81b5bd2f3238e9e16
IgnisFowl <fowl.ignis@gmail.com>
2025-09-20 16:13:56 -0300

    cpp02

--- cpp02/ex01/Fixed.cpp
+++ cpp02/ex01/Fixed.cpp
@@ -6,15 +6,33 @@
 /*   By: aarie-c2@c1r4p1.42sp.org.br <aarie-c2@c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/20 15:07:17 by aarie-c2@c1       #+#    #+#             */
-/*   Updated: 2025/09/20 15:07:17 by aarie-c2@c1      ###   ########.fr       */
+/*   Updated: 2025/09/20 16:13:02 by aarie-c2@c1      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include <iostream>
+#include <cmath>
 
 Fixed::Fixed() : _value(0) {
 	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int n) {
+	std::cout << "Int constructor called" << std::endl;
+	_value = n << _fractionalBits; 
+	/*bitwise left shift operator
+	Bit shifting left by 8 is the same as multiplying n by 
+	2 by 8 (256)*/
+}
+
+Fixed::Fixed(const float f) {
+    std::cout << "Float constructor called" << std::endl;
+    _value = static_cast<int>(roundf(f * (1 << _fractionalBits)));
+	/*This converts a floating-point number f into a fixed-point integer representation by:
+Scaling f by 2 elevated to fractionalBits (256) to move the decimal into the integer part.
+Rounding to the nearest integer to keep precision.
+Casting to integer for internal storage.*/
 }
 
 Fixed::Fixed(const Fixed& other) : _value(other._value) {
@@ -40,4 +58,17 @@ int Fixed::getRawBits(void) const {
 
 void Fixed::setRawBits(int const raw) {
 	_value = raw;
+}
+
+float Fixed::toFloat(void) const {
+	return static_cast<float>(_value) / (1 << _fractionalBits);
+}
+
+int Fixed::toInt(void) const {
+	return _value >> _fractionalBits;
+}
+
+std::ostream& operator<<(std::ostream& os, const Fixed& fixed) {
+	os << fixed.toFloat();
+	return os;
 }
--- cpp02/ex01/Fixed.hpp
+++ cpp02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: aarie-c2@c1r4p1.42sp.org.br <aarie-c2@c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/20 15:07:20 by aarie-c2@c1       #+#    #+#             */
-/*   Updated: 2025/09/20 15:07:21 by aarie-c2@c1      ###   ########.fr       */
+/*   Updated: 2025/09/20 15:09:03 by aarie-c2@c1      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,17 @@ class Fixed {
 		static const int _fractionalBits = 8;
 	public:
 		Fixed(); //default constructor
+		Fixed(const int n);
+		Fixed(const float f);
 		Fixed(const Fixed& other); //copy constructor
 		Fixed& operator = (const Fixed& other); //copy assignment operator
 		~Fixed(); //destructor
 
 		int getRawBits(void) const;
 		void setRawBits(int const raw);
+
+		float toFloat(void) const;
+		int toInt(void) const;
 };
 
 #endif
